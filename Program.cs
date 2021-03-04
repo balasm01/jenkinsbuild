f3cfc817e078a2a7d702b27da8594d00f2c911b9
balasm01 <maha.81@live.co.uk>
2021-03-04 21:52:24 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace balasm01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello groovy jenkins build");
+        }
+    }
+}
